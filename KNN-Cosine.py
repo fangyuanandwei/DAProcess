e33cea2c2c8da626516e8b315733c081a8b8b67c
fangyuanandwei <weiguanqun3578951@163.com>
2024-03-28 09:30:17 +0800

    Create KNN-Cosine.py

---
+++ KNN-Cosine.py
@@ -0,0 +1,20 @@
+    if args.distance == 'cosine':
+        all_fea = torch.cat((all_fea, torch.ones(all_fea.size(0), 1)), 1)
+        all_fea = (all_fea.t() / torch.norm(all_fea, p=2, dim=1)).t()
+
+    all_fea = all_fea.float().cpu().numpy()
+    K       = all_output.size(1)
+    aff     = all_output.float().cpu().numpy()
+
+    for _ in range(2):
+        initc = aff.transpose().dot(all_fea)
+        initc = initc / (1e-8 + aff.sum(axis=0)[:,None])
+        cls_count = np.eye(K)[predict].sum(axis=0)
+        labelset = np.where(cls_count>args.threshold)
+        labelset = labelset[0]
+
+        dd = cdist(all_fea, initc[labelset], args.distance)
+        pred_label = dd.argmin(axis=1)
+        predict    = labelset[pred_label]
+
+        aff = np.eye(K)[predict]
